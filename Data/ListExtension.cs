2cae3d128121a0b223d059e763416f075aee7ee9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-10-23 13:08:41 -0400

    Filled out a shuffle method for shuffling lists.

--- Data/ListExtension.cs
+++ Data/ListExtension.cs
@@ -1,9 +1,26 @@
-﻿namespace RVids.Data
+﻿using System;
+using System.Collections.Generic;
+
+namespace RVids.Data
 {
 
-    public class ListExtension
+    public static class ListExtension
     {
-        
+        // This is not random enough if you need extra security.
+        // For that use System.Cryptography. This is good enough for my purposes.
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rand = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
     }
 
 }
